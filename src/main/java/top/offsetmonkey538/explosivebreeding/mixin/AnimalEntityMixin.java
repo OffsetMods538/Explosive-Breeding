c9a98d25df2aac9a02aedcdda8a5c695341c9f0e
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-24 21:55:51 +0100

    Add animal entity mixin

---
+++ src/main/java/top/offsetmonkey538/explosivebreeding/mixin/AnimalEntityMixin.java
@@ -0,0 +1,10 @@
+package top.offsetmonkey538.explosivebreeding.mixin;
+
+import net.minecraft.entity.passive.AnimalEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(AnimalEntity.class)
+public abstract class AnimalEntityMixin {
+
+
+}
