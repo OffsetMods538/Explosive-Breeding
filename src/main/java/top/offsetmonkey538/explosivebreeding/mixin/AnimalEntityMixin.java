8e57556442b8d6ebb162489a2f26453ad7a999dc
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-24 21:59:07 +0100

    Make bred animals explode

--- src/main/java/top/offsetmonkey538/explosivebreeding/mixin/AnimalEntityMixin.java
+++ src/main/java/top/offsetmonkey538/explosivebreeding/mixin/AnimalEntityMixin.java
@@ -1,10 +1,21 @@
 package top.offsetmonkey538.explosivebreeding.mixin;
 
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AnimalEntity.class)
 public abstract class AnimalEntityMixin {
 
-
+    @Inject(
+            at = @At("HEAD"),
+            method = "breed"
+    )
+    private void explosivebreeding$makeBredAnimalsExplode(ServerWorld world, AnimalEntity other, CallbackInfo ci) {
+        world.createExplosion(null, null, null, other.getPos(), 4, false, World.ExplosionSourceType.MOB);
+    }
 }
